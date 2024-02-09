38dbfe612e7c2226a95cbf40295019d2f5c6fbf4
Hamish Roberts <hamish.jmr@gmail.com>
2024-02-09 21:02:19 +1300

    a few more todos. Added copy to clipboard function, along with page refresh

--- script.js
+++ script.js
@@ -3,11 +3,21 @@ backgroundColour = "#" + ("000000" + backgroundColour).slice(-6);
 document.body.style.backgroundColor = backgroundColour;
 
 if (backgroundColour === "") {
-    // TODO Add in if background color is dark, then invert the h1
+  // TODO Add in if background color is dark, then invert the h1
 } else {
   let colourID = document.getElementById("colour");
   colourID.innerHTML = backgroundColour.toUpperCase();
 }
 
+function copyToClipboard() {
+  navigator.clipboard.writeText(backgroundColour);
+  console.log("Copied the text: " + backgroundColour);
+}
+
+function pageRefresh() {
+  location.reload(true);
+}
+
 // TODO Make text copy to clipboard when clicked on
 // TODO Add other values to select (RGB etc.)
+// TODO Fix the ugly colour scheme...
